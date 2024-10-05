5931c3ccb95b0022727daa38f952f1b0f3401df8
JoseFrancisc0 <83974552+JoseFrancisc0@users.noreply.github.com>
2024-10-04 22:33:32 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,67 +1,66 @@
 import numpy as np
-import matplotlib.pyplot as plt
 import librosa
 import os
 import pandas as pd
 import seaborn as sns
+import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import confusion_matrix, accuracy_score, classification_report
-from svm import SVM
+from imblearn.over_sampling import SMOTE
+from sklearn.metrics import classification_report, confusion_matrix
+from knn import NearestNeighbor, get_best_k
+
+os.environ['LOKY_MAX_CPU_COUNT'] = '4'
 
 path = './cleaned_data/'
 
-def encode(path):
-  positives = []
-  positives_directory = os.listdir(path + "Positive")
-  print("Loading positives...")
-  for f in positives_directory:
-    y, sr = librosa.load(path + "Positive/" + f)
-    mfccs = librosa.feature.mfcc(y=y, sr=sr)
-    fv = mfccs.mean(axis = 1)
-    positives.append(fv)
-  positives_array = np.array(positives)
-  positives_array = np.insert(positives_array, 0, 1, axis=1)
-  print("Loading finished!")
+def encode(path, type1 = "Positive", type2 = "Negative"):
+    data1 = []
+    directory_1 = os.listdir(path + type1)
+    for f  in directory_1:
+        y, sr = librosa.load(path +  type1 + "/" + f)
+        mfccs = librosa.feature.mfcc(y=y, sr=sr)
+        fv = mfccs.mean(axis = 1)
+        data1.append(fv)
+    data = np.array(data1)
+    data = np.insert(data, 0, 1, axis=1)
 
-  negatives = []
-  negatives_directory = os.listdir(path + "test_neg")
-  print("Loading negatives...")
-  for f in negatives_directory:
-    y, sr = librosa.load(path + "test_neg/" + f)
-    mfccs = librosa.feature.mfcc(y=y, sr=sr)
-    fv = mfccs.mean(axis = 1)
-    negatives.append(fv)
-  negatives_array = np.array(negatives)
-  negatives_array = np.insert(negatives_array, 0, -1, axis=1)
-  print("Loading finished!")
+    data2 = []
+    directory_2 =  os.listdir(path + type2)
+    for f  in directory_2:
+        y, sr = librosa.load(path + type2 + "/" + f)
+        mfccs = librosa.feature.mfcc(y=y, sr=sr)
+        fv = mfccs.mean(axis = 1)
+        data2.append(fv)
+    temp = np.array(data2)
+    temp = np.insert(temp, 0, -1, axis=1)
 
-  print("Joining and shuffling loaded data...")
-  result_array = np.concatenate((positives_array, negatives_array), axis=0)
-  np.random.shuffle(result_array)
-  y = result_array[:,0]
-  x = result_array[:, 1:]
-  print("Done!")
-  return x, y
+    data = np.concatenate((data, temp), axis=0)
+    np.random.shuffle(data)
+    y = data[:,0]
+    x = data[:, 1:]
+    return x, y
+
+X, Y = encode(path)
+sm = SMOTE(random_state=42)
+X, Y = sm.fit_resample(X, Y)
+x_train, x_test, y_train, y_test = train_test_split(X, Y, test_size = 0.2, random_state = 42)
 
 def matriz_confusion(y_pred, y_test, Tipo):
-  matrix = confusion_matrix(y_test, y_pred)
-  f2 = pd.DataFrame(matrix.astype('float') / matrix.sum(axis=1)[:, np.newaxis], index=["Negative", 'Positive'], columns=["Negative", 'Positive'])
-  sns.heatmap(f2, annot=True, cbar=None, cmap="Greens")
-  plt.title("Confusion Matrix"  + Tipo ), plt.tight_layout()
-  plt.xlabel("Predicted")
-  plt.ylabel("Real")
-  plt.show()
+    matrix = confusion_matrix(y_test, y_pred)
+    f2 = pd.DataFrame(matrix.astype('float') / matrix.sum(axis=1)[:, np.newaxis], index=["Negative", "Positive"], columns=["Negative", "Positive"])
+    sns.heatmap(f2, annot=True, cbar=None, cmap="Greens")
+    plt.title("Confusion Matrix"  + Tipo ), plt.tight_layout()
+    plt.xlabel("Predicted")
+    plt.ylabel("Real")
+    plt.show()
 
-X_data, Y_data = encode(path)
-x_train, x_test, y_train, y_test = train_test_split(X_data, Y_data, test_size=0.3)
+# Train with K-fold
+k_fold = get_best_k(X, Y, 10)
+print(k_fold)
 
-model = SVM(1e8, 1e-10, 6000)
-model.train(x_train, y_train)
-y_pred = model.predict(x_test)
-print(y_pred)
-print(y_test)
-matriz_confusion(y_pred, y_test, " : SVM")
-
-report = classification_report(y_test, y_pred, target_names = ["Negative", "Positive"])
-print("Metrics")
-print(report)
+# Testing with own implementation
+knn = NearestNeighbor(2)
+knn.fit(x_train, y_train)
+y_pred = knn.predict(x_test)
+print(classification_report(y_test, y_pred, target_names=["Negative", "Positive"]))
+matriz_confusion(y_pred, y_test, " : KNN")
