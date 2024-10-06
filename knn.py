e6d2339f72836e58e23bf9069e53d8b806d6e66e
JoseFrancisc0 <83974552+JoseFrancisc0@users.noreply.github.com>
2024-10-06 01:55:39 -0500

    Update knn.py

--- knn.py
+++ knn.py
@@ -1,42 +1,45 @@
 from scipy.spatial import KDTree
 from scipy.stats import mode
-from sklearn.model_selection import cross_val_score, cross_val_predict
-from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import KFold
 from sklearn.neighbors import KNeighborsClassifier
-from sklearn.metrics import precision_score, recall_score, f1_score
+from sklearn.metrics import accuracy_score
 import numpy as np
 import pandas as pd
 
-### KNN implementation with K-fold with sklearn
+### Implementacion de knn con k-fold usando sklearn
 def KNN_Kfold(X, Y, k):
-    scaler = StandardScaler()
-    X_scaled = scaler.fit_transform(X)
+    kfold = KFold(n_splits=10)
+    knn = KNeighborsClassifier(n_neighbors=k, algorithm='kd_tree')
 
-    knn = KNeighborsClassifier(k)
-    y_pred = cross_val_predict(knn, X_scaled, Y, cv=10)
-    precision = precision_score(Y, y_pred, pos_label=1)
-    recall = recall_score(Y, y_pred, pos_label=1)
-    f1 = f1_score(Y, y_pred, pos_label=1)
+    accuracies = []
 
-    scores = cross_val_score(knn, X_scaled, Y, cv=5, scoring='accuracy')
-    mean_accuracy = np.mean(scores)
-    error = 1 - mean_accuracy
+    for train_index, test_index in kfold.split(X):
+        x_train, x_test = X[train_index], X[test_index]
+        y_train, y_test = Y[train_index], Y[test_index]
+        knn.fit(x_train, y_train)
+        y_pred = knn.predict(x_test)
 
-    return y_pred, mean_accuracy, precision, recall, f1, error
+        a = accuracy_score(y_test, y_pred)
+        accuracies.append(a)
+    
+    accuracy = np.mean(accuracies)
+    error = 1 - accuracy 
 
-### Handy function to calculate the best k
+    return accuracy, error
+    
+### Calcular k-fold a traves de multiples valores de k. El mejor se saca al ojo
 def get_best_k(X, Y, n):
     res = []
 
     for i in range(1, n+1):
-        y_pred, mean_accuracy, precision, recall, f1, error = KNN_Kfold(X, Y, i)
-        r = [i, round(mean_accuracy, 3), round(precision, 3), round(recall, 3), round(f1, 3), round(error, 3)]
+        mean_accuracy, mean_error = KNN_Kfold(X, Y, i)
+        r = [i, round(mean_accuracy, 2), round(mean_error, 2)]
         res.append(r)
 
-    df = pd.DataFrame(res, columns=['k', 'Mean Accuracy','Precision', 'Recall', 'F1 Score', 'Error'])
+    df = pd.DataFrame(res, columns=['k', 'Mean Accuracy', 'Mean Error'])
     return df
 
-### MANUAL IMPLEMENTATION OF KNN
+### IMPLEMENTACION DE KNN CON KDTREE
 class NearestNeighbor:
     def __init__(self, k=5):
         self.k = k
