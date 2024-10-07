082f8aebc2a306633e268448199c011d23bc7ff4
JoseFrancisc0 <83974552+JoseFrancisc0@users.noreply.github.com>
2024-10-07 00:12:07 -0500

    Update main.py

--- main.py
+++ main.py
@@ -7,11 +7,9 @@ import seaborn as sns
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import confusion_matrix, accuracy_score, classification_report
 from imblearn.over_sampling import SMOTE
-from svm import SVM
-from knn import KNN
-from logireg import LR
-from dtree import DT
-from bootstrap import *
+from knn import KNN_Kfold, run_KNN, NearestNeighbor
+from logireg import LR_Kfold, run_LR, LR
+from bootstrap import test_knn_bootstrapped, test_lr_bootstrapped
 
 np.random.seed(42)
 path = './tos/cleaned_data/'
@@ -62,57 +60,6 @@ X, Y = encode(path)
 sm = SMOTE(random_state=42)
 X, Y = sm.fit_resample(X, Y)
 x_train, x_test, y_train, y_test = train_test_split(X, Y, test_size = 0.3, random_state = 42)
-
-def test_svm():
-  print("Training SVM...")
-  model = SVM(1e8, 1e-10, 39000)
-  model.train(x_train, y_train)
-  y_pred = model.predict(x_test)
-  print(y_pred)
-  print(y_test)
-  matriz_confusion(y_pred, y_test, ": SVM")
-
-  report = classification_report(y_test, y_pred, target_names = ["Negative", "Positive"])
-  print("Metrics")
-  print(report)
-
-
-def test_knn():
-  print("Training KNN...")
-  model = KNN()
-  model.fit(x_train, y_train)
-  y_pred = model.predict(x_test)
-  print(y_pred)
-  print(y_test)
-  matriz_confusion(y_pred, y_test, ": KNN")
-
-  report = classification_report(y_test, y_pred, target_names = ["Negative", "Positive"])
-  print("Metrics")
-  print(report)
-
-def test_lr():
-  model = LR(1e-2, 1e6)
-  model.fit(x_train, y_train, 5000)
-  y_pred = model.predict(x_test)
-  print(y_pred)
-  print(y_test)
-  matriz_confusion(y_pred, y_test, ": LR")
-
-  report = classification_report(y_test, y_pred, target_names = ["Negative", "Positive"])
-  print("Metrics")
-  print(report)
-
-
-def test_dt():
-  model = DT(x_train, y_train)
-  y_pred = model.predict(x_test)
-  print(y_pred)
-  print(y_test)
-  matriz_confusion(y_pred, y_test, ": DT")
-
-  report = classification_report(y_test, y_pred, target_names = ["Negative", "Positive"])
-  print("Metrics")
-  print(report)
 
 ### LLAMA ESTA FUNCION PARA PROBAR EL MODELO
 def exec_knn():
@@ -166,6 +113,4 @@ def exec_lr():
 
 #exec_lr() ---> para ver la regresion logistica
 #exec_knn() ---> para ver el KNN
-
-
-test_knn_bootstrapped(x_train, y_train)
+# EL SVM Y EL DECISION TREE ESTAN EN EL PROJECT2 NOTEBOOK
