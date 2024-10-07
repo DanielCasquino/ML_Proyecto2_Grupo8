66058af3f8e522a35167ae6ccc6f82f63f4613ef
JoseFrancisc0 <83974552+JoseFrancisc0@users.noreply.github.com>
2024-10-07 00:02:12 -0500

    Update logireg.py

--- logireg.py
+++ logireg.py
@@ -26,11 +26,47 @@ To predict using the model, do::
   >>> model.predict(x_test)
 
 """
-
+from sklearn.metrics import classification_report
+from sklearn.model_selection import KFold
 import numpy as np
+import matplotlib.pyplot as plt
+
+### KFold for KNN
+def LR_Kfold(X, Y):
+    kfold = KFold(n_splits=10, shuffle=True, random_state=42)
+    results = np.zeros((10, 4))
+
+    i = 0
+    for train_index, test_index in kfold.split(X):
+        x_train, x_test = X[train_index], X[test_index]
+        y_train, y_test = Y[train_index], Y[test_index]
+
+        lr = LR(alpha=0.00001, lam=0.01)
+        lr.fit(x_train, y_train, epochs=5000)
+        y_pred = lr.predict(x_test)
+
+        report = classification_report(y_test, y_pred, target_names=["Negative", "Positive"], output_dict=True)
+        a = report['accuracy']
+        p = report['weighted avg']['precision']
+        r = report['weighted avg']['recall']
+        f = report['weighted avg']['f1-score']
+        results[i] = [a,p,r,f]
+        i += 1
+    
+    iterations = range(results.shape[0])
+
+    plt.plot(iterations, results[:, 0], label='Accuracy', color='red', linewidth=1)
+    plt.plot(iterations, results[:, 1], label='Precision', color='blue', linewidth=1)
+    plt.plot(iterations, results[:, 2], label='F1 Score', color='green', linewidth=1)
+    plt.plot(iterations, results[:, 3], label='Recall', color='purple', linewidth=1)
+
+    plt.title('10-fold Cross Validation Performance Metrics of LR')
+    plt.xlabel('Fold')
+    plt.ylabel('Scores')
+    plt.legend(loc="lower right")
+    plt.show()
 
 class LR:
-
   def __init__(self, alpha, lam):
     self.alpha = alpha
     self.lam = lam
@@ -66,4 +102,13 @@ class LR:
 
   def predict(self, x_test):
     prob = self.s(x_test)
-    return (prob >= np.median(prob)).astype(int)  
+    return (prob >= np.median(prob)).astype(int)
+
+def run_LR(x_train, x_test, y_train, y_test, lam):
+    lr = LR(alpha=0.00001, lam=lam)
+    lr.fit(x_train, y_train, epochs=5000)
+    y_pred = lr.predict(x_test)
+    report = classification_report(y_test, y_pred, target_names=["Negative", "Positive"], output_dict=True)
+    f1 = report['weighted avg']['f1-score']
+    return f1
+
